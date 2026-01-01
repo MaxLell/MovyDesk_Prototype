815f633414ec037b8fe289310825923305e055f8
maxkoell <maxkoell@proton.me>
2026-01-01 17:59:30 +0100

    added the c/c++ compiler guards

--- lib/FSM/FSM.h
+++ lib/FSM/FSM.h
@@ -1,30 +1,38 @@
 #ifndef FSM_H
 #define FSM_H
 
-
 #include "custom_types.h"
 
-typedef void (*fsm_state_action_cb)(void);
-
-typedef struct
+#ifdef __cplusplus
+extern "C"
 {
-  const u16 number_of_states;
-  const u16 number_of_events;
-  const u16 *transition_matrix;
-  const fsm_state_action_cb *state_actions;
-  u16 current_state;
-  u16 current_event;
-} fsm_config_t;
+#endif /* __cplusplus */
 
-void fsm_set_trigger_event(fsm_config_t *const config,
-                           u16 event);
+  typedef void (*fsm_state_action_cb)(void);
 
-void fsm_get_next_state(fsm_config_t *const config);
+  typedef struct
+  {
+    const u16 number_of_states;
+    const u16 number_of_events;
+    const u16 *transition_matrix;
+    const fsm_state_action_cb *state_actions;
+    u16 current_state;
+    u16 current_event;
+  } fsm_config_t;
 
-void fsm_run_state_action(const fsm_config_t *const config);
+  void fsm_set_trigger_event(fsm_config_t *const config,
+                             u16 event);
 
-void fsm_execute(fsm_config_t *const config);
+  void fsm_get_next_state(fsm_config_t *const config);
 
-void fsm_check_config(const fsm_config_t *const config);
+  void fsm_run_state_action(const fsm_config_t *const config);
+
+  void fsm_execute(fsm_config_t *const config);
+
+  void fsm_check_config(const fsm_config_t *const config);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif // FSM_H
