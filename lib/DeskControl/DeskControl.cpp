9d2037d7735e20517931dce27ddd8db523c3cf4a
maxkoell <maxkoell@proton.me>
2026-01-02 12:04:02 +0100

    Added a skeleton for the DesControl task

---
+++ lib/DeskControl/DeskControl.cpp
@@ -0,0 +1,26 @@
+#include "DeskControl.h"
+#include "custom_assert.h"
+
+// ###########################################################################
+// # Private function declarations
+// ###########################################################################
+
+// ###########################################################################
+// # Private variables
+// ###########################################################################
+
+// ###########################################################################
+// # Public function implementations
+// ###########################################################################
+
+void deskcontrol_init(void)
+{
+    // TODO: Initialize desk control hardware and state
+    // Placeholder for future implementation
+}
+
+void deskcontrol_run(void)
+{
+    // TODO: Main desk control processing loop
+    // Placeholder for future implementation
+}
---
+++ lib/DeskControl/DeskControl.h
@@ -0,0 +1,17 @@
+#ifndef DESKCONTROL_H
+#define DESKCONTROL_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+    void deskcontrol_init(void);
+
+    void deskcontrol_run(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif // DESKCONTROL_H
